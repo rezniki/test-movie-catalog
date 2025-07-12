0d4636208592b65d0380913ed3ee33d3ee620191
rezniki <igorreznik2001@gmail.com>
2025-07-12 22:07:16 +0300

    MovieList / created and add list movies

---
+++ src/components/MovieList.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="movie-list">
+        <movie-card v-for="movie in movies" :key="movie.imdbID" :movie="movie" />
+    </div>
+</template>
+
+<script setup>
+defineProps(['movies']);
+</script>
+
+<style scoped>
+.movie-list {
+    display: grid;
+    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+    gap: 20px;
+    padding: 20px;
+}
+</style>
