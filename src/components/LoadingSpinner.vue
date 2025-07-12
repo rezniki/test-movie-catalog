987e0d146e812ad50b5ffef7a132f6a37fd65535
rezniki <igorreznik2001@gmail.com>
2025-07-12 22:35:51 +0300

    LoadingSpinner / created and realization loading

---
+++ src/components/LoadingSpinner.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="spinner"></div>
+</template>
+
+<style scoped>
+.spinner {
+    border: 4px solid #f3f3f3;
+    border-top: 4px solid #1e90ff;
+    border-radius: 50%;
+    width: 40px;
+    height: 40px;
+    animation: spin 1s linear infinite;
+    margin: 20px auto;
+}
+
+@keyframes spin {
+    0% { transform: rotate(0deg); }
+    100% { transform: rotate(360deg); }
+}
+</style>
