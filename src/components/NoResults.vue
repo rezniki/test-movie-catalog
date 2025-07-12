4b1b8c7c43adaf5c75616324569b0a356a02fc5f
rezniki <igorreznik2001@gmail.com>
2025-07-12 22:39:56 +0300

    NoResults / created and add no-results

---
+++ src/components/NoResults.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="no-results">No results found</div>
+</template>
+
+<style scoped>
+.no-results {
+    text-align: center;
+    padding: 20px;
+    color: #666;
+    font-size: 18px;
+}
+</style>
